d024a8dc3b0544386974520ed701c2f4000482e9
navidmsk <navidmsk@yahoo.com>
2014-08-23 03:04:49 +0430

    "add MainClass.java for check"

---
+++ MainClass.java
@@ -0,0 +1,24 @@
+package contactList;
+
+public class MainClass {
+
+	private String lable;
+
+	private String string;
+
+	public String getString() {
+		return string;
+	}
+
+	void setString(String string) {
+		this.string = string;
+	}
+	
+	public String getLable() {
+		return lable;
+	}
+	
+	void setLable(String lable) {
+		this.lable = lable;
+	}
+}
