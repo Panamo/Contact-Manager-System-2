c1e0daa69585da28fe4535847caf48f193cc8b5f
navidmsk <navidmsk@yahoo.com>
2014-08-23 03:01:27 +0430

    "add ContactList.java for check"

---
+++ ContactList.java
@@ -0,0 +1,20 @@
+package contactList;
+
+import java.util.ArrayList;
+
+public class ContactList {
+
+	ArrayList<Contact> contact;
+	
+	public ContactList() {
+		contact = new ArrayList<>();
+	}
+
+	public ArrayList<Contact> getContact() {
+		return contact;
+	}
+	
+	void addToContact(Contact contact) {
+		this.contact.add(contact);
+	}
+}
